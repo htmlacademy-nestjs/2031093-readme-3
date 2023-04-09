96301b8c967cb4fbb86303017b47888d59e2507d
VitalSTU <kolysvit@mail.ru>
2023-04-09 21:51:38 +0300

    2.11. BlogUserEntity created

---
+++ project/apps/users/src/app/blog-user/blog-user.entity.ts
@@ -0,0 +1,27 @@
+import { User } from '@project/shared/app-types';
+
+export class BlogUserEntity implements User {
+  public _id: string;
+  public email: string;
+  public firstname: string;
+  public lastname: string;
+  public passwordHash: string;
+  public avatar: string;
+
+  constructor(blogUser: User) {
+    this.fillEntity(blogUser);
+  }
+
+  public toObject() {
+    return {...this};
+  }
+
+  public fillEntity(blogUser: User) {
+    this._id = blogUser._id;
+    this.email = blogUser.email;
+    this.firstname = blogUser.firstname;
+    this.lastname = blogUser.lastname;
+    this.passwordHash = blogUser.passwordHash;
+    this.avatar = blogUser.avatar;
+  }
+}
