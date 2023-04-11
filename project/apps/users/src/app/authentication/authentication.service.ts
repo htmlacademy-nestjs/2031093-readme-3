ca1f099d73f4d8355d846d9a3997732bca90b9e2
VitalSTU <kolysvit@mail.ru>
2023-04-11 21:33:07 +0300

    2.22. User verification method added to AuthenticationService.

--- project/apps/users/src/app/authentication/authentication.service.ts
+++ project/apps/users/src/app/authentication/authentication.service.ts
@@ -1,10 +1,20 @@
-import { Injectable, ConflictException } from '@nestjs/common';
+import {
+  Injectable,
+  ConflictException,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import dayjs from 'dayjs';
 
 import { BlogUserMemoryRepository } from '../blog-user/blog-user-memory.repository';
 import { BlogUserEntity } from '../blog-user/blog-user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
-import { AUTH_USER_EXISTS } from './authentication.constant';
+import { LoginUserDto } from './dto/login-user.dto';
+import {
+  AUTH_USER_EXISTS,
+  AUTH_USER_NOT_FOUND,
+  AUTH_USER_PASSWORD_WRONG,
+} from './authentication.constant';
 
 @Injectable()
 export class AuthenticationService {
@@ -32,5 +42,25 @@ export class AuthenticationService {
     const userEntity = await new BlogUserEntity(blogUser).setPassword(password);
 
     return this.blogUserRepository.create(userEntity);
+  }
+
+  public async verifyUser(dto: LoginUserDto) {
+    const {email, password} = dto;
+    const existUser = await this.blogUserRepository.findByEmail(email);
+
+    if (!existUser) {
+      throw new NotFoundException(AUTH_USER_NOT_FOUND);
+    }
+
+    const blogUserEntity = new BlogUserEntity(existUser);
+    if (!await blogUserEntity.comparePassword(password)) {
+      throw new UnauthorizedException(AUTH_USER_PASSWORD_WRONG);
+    }
+
+    return blogUserEntity.toObject();
+  }
+
+  public async getUser(id: string) {
+    return this.blogUserRepository.findById(id);
   }
 }
