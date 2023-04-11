3a3693770fdfc2826a8b911d277709f900e09ae5
VitalSTU <kolysvit@mail.ru>
2023-04-11 21:54:24 +0300

    2.26. /login and /:id routes processors added to authentication controller.

--- project/apps/users/src/app/authentication/authentication.controller.ts
+++ project/apps/users/src/app/authentication/authentication.controller.ts
@@ -1,9 +1,11 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Param } from '@nestjs/common';
 
 import { AuthenticationService } from './authentication.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
 import { fillObject } from '@project/util/util-core';
 import { UserRdo } from './rdo/user.rdo';
+import { LoggedUserRdo } from './rdo/logged-user.rdo';
 
 @Controller('authentication')
 export class AuthenticationController {
@@ -15,5 +17,17 @@ export class AuthenticationController {
   public async create(@Body() dto: CreateUserDto) {
     const newUser = await this.authService.register(dto);
     return fillObject(UserRdo, newUser);
+  }
+
+  @Post('login')
+  public async login(@Body() dto: LoginUserDto) {
+    const verifiedUser = await this.authService.verifyUser(dto);
+    return fillObject(LoggedUserRdo, verifiedUser);
+  }
+
+  @Get(':id')
+  public async show(@Param('id') id: string) {
+    const existUser = await this.authService.getUser(id);
+    return fillObject(UserRdo, existUser);
   }
 }
