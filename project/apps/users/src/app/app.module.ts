019a82425df120a6e113a0d0e74fddd5f493d155
VitalSTU <kolysvit@mail.ru>
2023-04-20 22:16:27 +0300

    3.17. MongooseModule imported into users service root module.

--- project/apps/users/src/app/app.module.ts
+++ project/apps/users/src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { BlogUserModule } from './blog-user/blog-user.module';
 import { AuthenticationModule } from './authentication/authentication.module';
-import { ConfigUsersModule } from '@project/config/config-users';
+import { ConfigUsersModule, getMongooseOptions } from '@project/config/config-users';
 
 @Module({
-  imports: [BlogUserModule, AuthenticationModule, ConfigUsersModule],
+  imports: [
+    BlogUserModule,
+    AuthenticationModule,
+    ConfigUsersModule,
+    MongooseModule.forRootAsync(
+      getMongooseOptions()
+  )],
   controllers: [],
   providers: [],
 })
