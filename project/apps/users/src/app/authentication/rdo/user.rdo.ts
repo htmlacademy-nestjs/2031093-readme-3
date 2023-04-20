c0e811b2278f7b482010a5124101c3dda7928007
VitalSTU <kolysvit@mail.ru>
2023-04-20 23:07:25 +0300

    fix: User RDO ObjectId transformation to string added.

--- project/apps/users/src/app/authentication/rdo/user.rdo.ts
+++ project/apps/users/src/app/authentication/rdo/user.rdo.ts
@@ -1,5 +1,6 @@
-import { Expose } from 'class-transformer';
 import { ApiProperty } from '@nestjs/swagger';
+
+import { Expose, Transform } from 'class-transformer';
 
 export class UserRdo {
   @ApiProperty({
@@ -7,6 +8,7 @@ export class UserRdo {
     example: '13'
   })
   @Expose({ name: '_id'})
+  @Transform(({obj}) => obj._id.toString())
   public id: string;
 
   @ApiProperty({
